a35ffeac3791b9eaf62d4bcad49d1539b050855b
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-03-27 15:45:20 +0200

    Persist sensor data in FIWARE.

--- src/main/java/de/app/fivegla/integration/fiware/FiwareIntegrationServiceWrapper.java
+++ src/main/java/de/app/fivegla/integration/fiware/FiwareIntegrationServiceWrapper.java
@@ -4,9 +4,14 @@ package de.app.fivegla.integration.fiware;
 import de.app.fivegla.api.Constants;
 import de.app.fivegla.fiware.DeviceIntegrationService;
 import de.app.fivegla.fiware.DeviceMeasurementIntegrationService;
+import de.app.fivegla.fiware.api.enums.DeviceCategoryValues;
+import de.app.fivegla.fiware.model.Device;
+import de.app.fivegla.fiware.model.DeviceCategory;
+import de.app.fivegla.integration.farm21.model.Sensor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -32,4 +37,17 @@ public class FiwareIntegrationServiceWrapper {
         return Constants.FIWARE_FARM21_SENSOR_ID_PREFIX + UUID.randomUUID();
     }
 
+    /**
+     * Create a new Farm21 sensor in FIWARE.
+     * @param sensor
+     */
+    public void persist(Sensor sensor) {
+        var device = Device.builder()
+                .id(getFiwareId(sensor.getId()))
+                .deviceCategory(DeviceCategory.builder()
+                        .value(List.of(DeviceCategoryValues.Farm21Sensor.getKey()))
+                        .build())
+                .build();
+        deviceIntegrationService.persist(device);
+    }
 }
